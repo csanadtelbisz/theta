a6297a87d5d31b8370edecfb672f5bf5e7b62a19
Csanád Telbisz <csanadtelbisz@gmail.com>
2023-04-27 00:33:02 +0200

    AALBE thread operations fix

--- subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/por/XcfaLbeLts.kt
+++ subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/por/XcfaLbeLts.kt
@@ -7,10 +7,10 @@ import hu.bme.mit.theta.xcfa.analysis.XcfaAction
 import hu.bme.mit.theta.xcfa.analysis.XcfaState
 import hu.bme.mit.theta.xcfa.analysis.getCoreXcfaLts
 import hu.bme.mit.theta.xcfa.getAtomicBlockInnerLocations
+import hu.bme.mit.theta.xcfa.getFlatLabels
 import hu.bme.mit.theta.xcfa.getGlobalVars
-import hu.bme.mit.theta.xcfa.model.SequenceLabel
-import hu.bme.mit.theta.xcfa.model.XCFA
-import hu.bme.mit.theta.xcfa.model.XcfaEdge
+import hu.bme.mit.theta.xcfa.model.*
+import hu.bme.mit.theta.xcfa.passes.changeVars
 
 class XcfaLbeLts(private val xcfa: XCFA) : LTS<XcfaState<out ExprState>, XcfaAction> {
     companion object {
@@ -26,27 +26,29 @@ class XcfaLbeLts(private val xcfa: XCFA) : LTS<XcfaState<out ExprState>, XcfaAct
         val enabledActions = coreXcfaLts.getEnabledActionsFor(state)
         return enabledActions.map { action ->
             var edge = action.edge
-            val labels = mutableListOf(edge.label)
+            val labels = mutableListOf(action.label)
             var globalEdgeReached = edge.isGlobal(prec)
             while (edge.target.outgoingEdges.size == 1 && (edge.target in atomicBlockInnerLocations || !globalEdgeReached)) {
-                edge = edge.next
-                if (edge.isGlobal(prec)) {
+                if (edge.next.isGlobal(prec)) {
                     globalEdgeReached = true
-                    if (edge.target !in atomicBlockInnerLocations) break
+                    if (edge.next.target !in atomicBlockInnerLocations) break
                 }
-                labels.add(edge.label)
+                edge = edge.next
+                val newLabel = edge.label.changeVars(state.processes[action.pid]!!.varLookup.peek())
+                labels.add(newLabel)
             }
             XcfaAction(
                 pid = action.pid,
                 source = action.edge.source,
                 target = edge.target,
-                label = if (labels.size == 1) labels.first() else SequenceLabel(labels)
+                label = SequenceLabel(labels.flatMap { if (it is SequenceLabel) it.labels else listOf(it) })
             )
         }.toSet()
     }
 
     private fun <P : Prec> XcfaEdge.isGlobal(prec: P) =
-        (this.getGlobalVars(xcfa).keys intersect prec.usedVars.toSet()).isNotEmpty()
+        getFlatLabels().any { it is StartLabel || it is JoinLabel || it is InvokeLabel } ||
+                (this.getGlobalVars(xcfa).keys intersect prec.usedVars.toSet()).isNotEmpty()
 
     private val XcfaEdge.next get() = target.outgoingEdges.first()
 }
